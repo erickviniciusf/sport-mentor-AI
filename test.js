1669a4899e3673da59ab2abd7d62ae5d0f555435
Erick Vinicius <erick.viniciusfff@gmail.com>
2026-05-26 13:45:36 -0300

    update

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-import { getMatchContext } from './src/services/aiService.js';
-
-const result = await getMatchContext('São Paulo', 'Botafogo');
-console.log('ANÁLISE:');
-console.log(result.analysis);
-console.log('\nFONTES:');
-result.sources.forEach(s => console.log('·', s));
+console.loggit
